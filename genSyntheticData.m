7d0f1f8c0b268f5c9114d34d1a04a8c084819b4d
Mark White <markgewhite@gmail.com>
2021-12-12 09:35:08 +0000

    First version

---
+++ genSyntheticData.m
@@ -0,0 +1,95 @@
+% ************************************************************************
+% Function: genFunctionalData
+%
+% Generate synthetic functional data 
+% based originally on the method proposed by Hsieh et al. (2021).
+%
+% Enhanced with option to have multiple basis levels.
+% The number of levels is specified if basis is a cell array
+% 
+% Parameters:
+%           nObs    : number of observations per class (vector)
+%           nDim    : number of dimensions
+%           basis   : functional basis 
+%                     (data object or cell array of data objects)
+%           beta    : array of proportional factors between levels
+%                     (must match the number basis elements)
+%           
+% Outputs:
+%           X     : synthetic time series
+%           XFd   : functional synthetic data
+% ************************************************************************
+
+function [ X, XFd ] = genSyntheticData( nObs, nDim, basis, beta )
+
+% prepare (optionally) for multiple basis layers
+if isa( basis, 'basis')
+    nLevels = 1;
+    basisFd{1} = basis;
+else
+    nLevels = length( basis );
+    basisFd = cell( nLevels, 1 );
+    for i = 1:nLevels
+        basisFd{i} = basis{i};
+    end
+end
+
+% initialise the number of coefficients and spacing
+nCoeff = zeros( nLevels, 1 );
+spacing = cell( nLevels, 1 );
+for i = 1:nLevels
+    nCoeff(i) = getnbasis( basisFd{i} );
+    range = getbasisrange( basisFd{i} );
+    spacing{i} = linspace( range(1), range(2), nCoeff(i) )';
+end
+
+X = zeros( nCoeff(1), sum(nObs), nDim );
+
+a = 0;
+for c = 1:length(nObs)
+
+    % generate random template function coefficients
+    % with covariance between the dimensions
+    
+    coeff = randSeries( nCoeff(1), nDim );
+    % add interpolated layers (if required)
+    for i = 2:nLevels
+        coeffi = randSeries( nCoeff(i), nDim );
+        for j = 1:nDim
+            coeff(:,j) = coeff(:,j) + ...
+                           beta(i-1)*interp1( spacing{i}, ...
+                                              coeffi(:,j), spacing{1}, ...
+                                              'cubic' );
+        end
+    end
+    
+    for i = 1:nObs(c)
+       
+        % vary the template function 
+        % with covariance between series elements
+        a = a+1;
+        X( :, a, : ) = coeff + beta(end)*randSeries( nDim, nCoeff(1) )';
+               
+    end
+
+end
+
+XFd = fd( X, basisFd{1} );
+
+end
+
+
+function Z = randSeries( n, d )
+
+    % generate Gaussian random matrix
+    R = randn( d );
+    % get the covariance matrix with a fractional offset to avoid errors
+    sigma = cov( R ) + 1E-6*eye( d );
+    % get the means
+    mu = mean( R );
+    
+    % generate coefficients from this random multivariate distribution
+    Z = mvnrnd( mu, sigma, n );
+
+
+end
---
+++ testSyntheticData.m
@@ -0,0 +1,30 @@
+% ************************************************************************
+% Script: test synthetic data
+%
+% ************************************************************************
+
+setup.nBasis = 43;
+setup.basisOrder = 4;
+setup.penaltyOrder = 2;
+setup.lambda = 10E0;
+
+tSpan = linspace( 0, 1000, 100 );
+
+basis = create_bspline_basis( [ tSpan(1), tSpan(end) ], ...
+                              setup.nBasis, ...
+                              setup.basisOrder);
+                          
+XFdPar = fdPar( basis, setup.penaltyOrder, setup.lambda ); 
+
+basisHL = create_bspline_basis( [ tSpan(1), tSpan(end) ], ...
+                              (setup.nBasis-3)/4+3, ...
+                              setup.basisOrder);
+
+basisHL2 = create_bspline_basis( [ tSpan(1), tSpan(end) ], ...
+                              (setup.nBasis-3)/8+3, ...
+                              setup.basisOrder);
+
+[X, XFd] = genSyntheticData( [ 20 20 20 ], 3, ...
+                                { basis, basisHL, basisHL2 }, [10 2 5] );
+
+plot( XFd );
