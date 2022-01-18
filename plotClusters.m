3c1a57b8768d20e1e6b5d411e5a14ebe582e7994
Mark White <markgewhite@gmail.com>
2022-01-18 09:25:56 +0000

    Modified to accommodate different calling arguments

--- plotClusters.m
+++ plotClusters.m
@@ -10,43 +10,71 @@
 %           CHat        : predicted classes
 % ************************************************************************
 
-function plotClusters( ax, Z, C, CHat )
+function plotClusters( ax, Z, C, CHat, compact )
+
+if nargin>=4
+    if isa( CHat, 'logical' )
+        inclPredictedClass = false;
+        compact = CHat;
+    else
+        inclPredictedClass = true;
+        if nargin==4
+            compact = false;
+        end
+    end
+else
+    inclPredictedClass = false;
+    compact = false;
+end
 
 C = categorical( C );
-CHat = categorical( CHat );
-
 class = sort( unique( C ) );
 
 cla( ax, 'reset' );
 hold( ax, 'on' );
 
+if compact
+    dotSize = 10;
+    dotSize2 = 4;
+else
+    dotSize = 40;
+    dotSize2 = 10;
+end
+
 % plot true classes (large dots)
 ax.ColorOrderIndex = 1;
 for i = 1:length( class )
     idx = (C==class(i));
-    scatter( ax, Z(idx,1), Z(idx,2), 40, 'filled' );
+    scatter( ax, Z(idx,1), Z(idx,2), dotSize, 'filled' );
 end
 
-% plot estimated classes (small dots on top)
-ax.ColorOrderIndex = 1;
-for i = 1:length( class )
-    idx = (CHat==class(i));
-    scatter( ax, Z(idx,1), Z(idx,2), 10, 'filled' );
+if inclPredictedClass
+    % plot estimated classes (small dots on top)
+    CHat = categorical( CHat );
+    ax.ColorOrderIndex = 1;
+    for i = 1:length( class )
+        idx = (CHat==class(i));
+        scatter( ax, Z(idx,1), Z(idx,2), dotSize2, 'filled' );
+    end
 end
 
-% then add the centroids on top
-for i = 1:length( class )
-    idx = (C==class(i));
-    text( ax, mean( Z(idx,1) ), mean( Z(idx,2) ), ...
-              class(i), ...
-              'HorizontalAlignment', 'center', ...
-              'FontWeight', 'bold', ...
-              'FontSize', 10, ...
-              'Color', [0 0 0] );
+if ~compact
+    % then add the centroids on top
+    for i = 1:length( class )
+        idx = (C==class(i));
+        text( ax, mean( Z(idx,1) ), mean( Z(idx,2) ), ...
+                  class(i), ...
+                  'HorizontalAlignment', 'center', ...
+                  'FontWeight', 'bold', ...
+                  'FontSize', 10, ...
+                  'Color', [0 0 0] );
+    end
 end
 
 hold( ax, 'off' );
 
-legend( ax, class, 'Location', 'Best' );
+if ~compact
+    legend( ax, class, 'Location', 'Best' );
+end
 
 end
