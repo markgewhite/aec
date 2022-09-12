f11fd4ce9326f549bc33646d4601487b350ea9b2
Mark White <markgewhite@gmail.com>
2022-09-12 20:01:49 +0100

    Removed FullAEModel

--- code/model/@FullAEModel/private/getFcnNames.m
+++
@@ -1,9 +0,0 @@
-function names = getFcnNames( lossFcns )
-
-    nFcns = length( lossFcns );
-    names = strings( nFcns, 1 );
-    for i = 1:nFcns
-        names(i) = lossFcns{i}.Name;
-    end
-
-end
