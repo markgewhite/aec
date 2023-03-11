7138ac5260c426a0d3372c2afe1b9838e7b3540a
Mark White <markgewhite@gmail.com>
2023-03-11 07:15:45 +0000

    Updated arrangeComponents for ZDimAux

--- code/model/@ModelEvaluation/arrangeComponents.m
+++ code/model/@ModelEvaluation/arrangeComponents.m
@@ -6,11 +6,11 @@ function self = arrangeComponents( self )
     end
 
     aModel = self.Models{1};
-    if aModel.ZDim > 10
+    if aModel.ZDimAux > 10
         % astronmical number of permutations
         return
     end
-    permOrderIdx = perms( 1:aModel.ZDim );
+    permOrderIdx = perms( 1:aModel.ZDimAux );
     lb = [ length(permOrderIdx) ones( 1, self.NumModels-1 ) ];
     ub = length(permOrderIdx)*ones( 1, self.NumModels );
     options = optimoptions( 'ga', ...
@@ -26,7 +26,7 @@ function self = arrangeComponents( self )
     % pre-compile latent components across the sub-models for speed
     XDim = size( aModel.LatentComponents, 1 );
     latentComp = zeros( XDim, aModel.NumCompLines, ...
-                        aModel.ZDim, aModel.XChannels, self.NumModels );
+                        aModel.ZDimAux, aModel.XChannels, self.NumModels );
     for k = 1:self.NumModels
         latentComp(:,:,:,:,k) = self.Models{k}.LatentComponents;
     end
@@ -40,7 +40,7 @@ function self = arrangeComponents( self )
                             lb, ub, [], 1:self.NumModels, options );
 
     % generate the order from list of permutations
-    self.ComponentOrder = zeros( self.NumModels, aModel.ZDim );
+    self.ComponentOrder = zeros( self.NumModels, aModel.ZDimAux );
     for k = 1:self.NumModels
         self.ComponentOrder( k, : ) = permOrderIdx( componentPerms(k), : );
     end
